9582ccf39f3f8cfe9d6882b5d2e266ff8fd62618
dxalmutti52-ux <dxalmutti52@gmail.com>
2026-05-07 16:06:52 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CodeFirstStudentApp.Models;
+
+namespace CodeFirstStudentApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new StudentContext())
+            {
+                var student = new Student
+                {
+                    Name = "John Doe",
+                    Age = 22
+                };
+
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                Console.WriteLine("Student added successfully!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
