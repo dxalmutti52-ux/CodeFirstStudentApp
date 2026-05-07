5e068fc339c9ad21fcdc0369aa3f773e11513385
dxalmutti52-ux <dxalmutti52@gmail.com>
2026-05-07 16:04:48 -0400

    Add files via upload

---
+++ Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeFirstStudentApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }   // Primary Key
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Models/StudentContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using CodeFirstStudentApp.Models;
+
+namespace CodeFirstStudentApp
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext() : base("StudentDBConnection")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
